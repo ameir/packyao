db6b9032b840836542e42496b7a82275844c2b9a
Ameir Abdeldayem <ameir.abdeldayem@teamaol.com>
2015-07-03 04:04:12 -0400

    Add default package metadata. Run rubocop.

--- packyao.rb
+++ packyao.rb
@@ -4,80 +4,76 @@ require 'json'
 def run_command(command)
   puts "running command '#{command}'..."
   system(command)
-  puts "Returned: #{$?}"
+  puts "Returned: #{$CHILD_STATUS}"
 end
 
 # TODO: checkout specific branch or commit
-def git_clone(url, branch_or_commit = 'master')
-  commands = ['git init', 'git remote add origin ' + $url, 'get pull'];
+def git_clone(_url, _branch_or_commit = 'master')
+  commands = ['git init', 'git remote add origin ' + url, 'get pull']
   commands.each do |command|
-    run_command(command);
+    run_command(command)
   end
 end
 
 def http_download(url)
-  command = "wget -O source '#{url}'";
-  run_command(command);
+  command = "wget -O source '#{url}'"
+  run_command(command)
 end
 
 def build_package(params)
-
   puts "Currently in #{Dir.pwd}"
 
   # set env vars
-  params['env'].each do |key,value|
-    puts "exporting #{key}=#{value}";
+  params['env'].each do |key, value|
+    puts "exporting #{key}=#{value}"
     ENV[key] = value
   end
 
   params['commands'].each do |command|
-    run_command(command);
+    run_command(command)
   end
 end
 
 def create_package_layout(params)
-
   require 'fileutils'
-  workspace = 'packyao-dist';
-  Dir.mkdir(workspace, 0777) unless Dir.exists?(workspace)
+  workspace = 'packyao-dist'
+  Dir.mkdir(workspace, 0777) unless Dir.exist?(workspace)
 
   puts "Currently in #{Dir.pwd}"
-  params['package_files'].each do |source,destination|
+  params['package_files'].each do |source, destination|
     puts "Processing: #{source} -> #{destination}"
     destination_dir = File.dirname(workspace + destination)
     puts "Creating directory '#{destination_dir}'..."
-    FileUtils.mkdir_p(destination_dir) unless Dir.exists?(destination_dir)
+    FileUtils.mkdir_p(destination_dir) unless Dir.exist?(destination_dir)
     puts "Copying: #{source} -> #{workspace + destination}"
     FileUtils.cp(source, workspace + destination)
   end
 end
 
 def create_package(params)
-
-  require "fpm"
-  require "fpm/command"
+  require 'fpm'
+  require 'fpm/command'
 
   # https://github.com/tim-group/deployapp/blob/master/Rakefile
   arguments = [
-    "-n", params['name'],
-    "-v", params['version'],
-    "-m", "packyao <ameirh@gmail.com>",
-    "-a", 'all',
-    "-t", 'deb',
-    "-s", 'dir',
-    "--description", "description",
-    "--url", 'http://www.packyao.com',
-    "-C", 'packyao-dist'
+    '-n', params['name'],
+    '-v', params['version'],
+    '-m', params['maintainer'] || 'packyao <ameirh+packyao@gmail.com>',
+    '-t', 'deb',
+    '-s', 'dir',
+    '--description', params['description'] || 'This package was created by packyao.',
+    '--url', params['url'] || 'http://www.packyao.com',
+    '-C', 'packyao-dist'
   ]
 
-  fail "problem creating debian package" unless FPM::Command.new('fpm').run(arguments) == 0
+  fail 'problem creating package' unless FPM::Command.new('fpm').run(arguments) == 0
 end
 
 filename = 'packyao.json'
 params = JSON.parse(File.read(filename))
 
-workspace = 'packyao-workspace';
-Dir.mkdir(workspace, 0777) unless Dir.exists?(workspace)
+workspace = 'packyao-workspace'
+Dir.mkdir(workspace, 0777) unless Dir.exist?(workspace)
 Dir.chdir(workspace)
 
 case params['type']
