cabe23544e6726ba4c6044a2ef314e114fa94725
Ameir Abdeldayem <ameir.abdeldayem@teamaol.com>
2015-07-02 20:49:52 -0400

    Begin creating packages with FPM.

--- packyao.rb
+++ packyao.rb
@@ -7,7 +7,8 @@ def run_command(command)
   puts "Returned: #{$?}"
 end
 
-def git_clone(url)
+# TODO: checkout specific branch or commit
+def git_clone(url, branch_or_commit = 'master')
   commands = ['git init', 'git remote add origin ' + $url, 'get pull'];
   commands.each do |command|
     run_command(command);
@@ -34,7 +35,45 @@ def build_package(params)
   end
 end
 
-filename = 'packyao-twemproxy.json'
+def create_package_layout(params)
+
+  require 'fileutils'
+  workspace = 'packyao-dist';
+  Dir.mkdir(workspace, 0777) unless Dir.exists?(workspace)
+
+  puts "Currently in #{Dir.pwd}"
+  params['package_files'].each do |source,destination|
+    puts "Processing: #{source} -> #{destination}"
+    destination_dir = File.dirname(workspace + destination)
+    puts "Creating directory '#{destination_dir}'..."
+    FileUtils.mkdir_p(destination_dir) unless Dir.exists?(destination_dir)
+    puts "Copying: #{source} -> #{workspace + destination}"
+    FileUtils.cp(source, workspace + destination)
+  end
+end
+
+def create_package(params)
+
+  require "fpm"
+  require "fpm/command"
+
+  # https://github.com/tim-group/deployapp/blob/master/Rakefile
+  arguments = [
+    "-n", params['name'],
+    "-v", params['version'],
+    "-m", "packyao <ameirh@gmail.com>",
+    "-a", 'all',
+    "-t", 'deb',
+    "-s", 'dir',
+    "--description", "description",
+    "--url", 'http://www.packyao.com',
+    "-C", 'packyao-dist'
+  ]
+
+  fail "problem creating debian package" unless FPM::Command.new('fpm').run(arguments) == 0
+end
+
+filename = 'packyao.json'
 params = JSON.parse(File.read(filename))
 
 workspace = 'packyao-workspace';
@@ -51,3 +90,5 @@ else
 end
 
 build_package(params)
+create_package_layout(params)
+create_package(params)
